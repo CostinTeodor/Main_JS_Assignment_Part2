9e0ed416a3c243e3b529526dd0d5298badf765a4
CostinTeodor <41041994+CostinTeodor@users.noreply.github.com>
2022-09-08 21:01:30 +0300

    First Attempt
    Almost no styling, didn't check for indentation

---
+++ scripts/index.js
@@ -0,0 +1,154 @@
+let resultTextElement = document.querySelector("#result");
+
+const addButton = document.querySelector("#addition");
+const substractButton = document.querySelector("#substraction");
+const multiplyButton = document.querySelector("#multiplication");
+const divideButton = document.querySelector("#division");
+
+const allButtons = document.querySelectorAll("button");
+
+allButtons.forEach(element => {
+    element.addEventListener("click", event => {
+        event.preventDefault();
+    })
+});
+
+const operationsButtons = document.querySelectorAll(".operations-buttons");
+
+const screenElements = document.querySelectorAll(".screening-text");
+
+operationsButtons.forEach(element => {
+    element.addEventListener("click", event => {
+        if (resultTextElement.textContent == "")
+            resultTextElement.textContent = "0";
+    })
+});
+
+screenElements.forEach(element => {
+    element.addEventListener("click", event => {
+        if (checkEnding() &&
+            element.classList.contains("operations-buttons"))
+            resultTextElement.textContent = resultTextElement.textContent.slice(0, -1);
+        resultTextElement.textContent += element.textContent;
+    })
+});
+
+function calculate(string) {
+    let result = eval(string);
+    result = ""+((Math.round(result * 100) / 100).toFixed(2));
+    if(result.endsWith(".00"))
+        result = result.slice(0, -3);
+    else if(result.endsWith("0") && result[result.length-3] == ".")
+        result = result.slice(0,-1);
+    return result;
+}
+
+const equalButton = document.querySelector("#equal");
+
+equalButton.addEventListener("click", event => {
+    equalButtonFunction();
+});
+
+const clearButton = document.querySelector("#clear");
+
+clearButton.addEventListener("click", event => {
+    clearButtonFunction();
+});
+
+const backspaceButton = document.querySelector("#backspace");
+
+backspaceButton.addEventListener("click", event => {
+    backspaceButtonFunction();
+});
+
+const floatPoint = document.querySelector("#float-point");
+
+floatPoint.addEventListener("click", event => {
+    floatPointFunction();
+});
+
+function inArray(array, item) {
+    if (array.includes(item))
+        return true;
+    return false;
+}
+
+let separators = ['+', '-', '/', '*', '.', 'Enter', 'Backspace', 'Escape'];
+
+document.body.addEventListener("keyup", event => {
+    if(resultTextElement.textContent.length >=30){
+        alert("Largest operation is 30 characters long!");
+        resultTextElement.textContent = resultTextElement.textContent.slice(0, -1);
+    }
+    if (!isNaN(event.key) || separators.indexOf(event.key) != -1) {
+        switch (event.key) {
+            case ".":
+                if (resultTextElement.textContent == "")
+                    resultTextElement.textContent = "0";
+                floatPointFunction();
+                break;
+            case "+":
+            case "/":
+            case "*":
+            case "-":
+                if (resultTextElement.textContent == "")
+                    resultTextElement.textContent = "0";
+                if (checkEnding() && isNaN(resultTextElement.textContent[resultTextElement.textContent.length - 1]))
+                    resultTextElement.textContent = resultTextElement.textContent.slice(0, -1);
+                resultTextElement.textContent += event.key;
+                break;
+            case 'Enter':
+                equalButtonFunction();
+                break;
+            case 'Backspace':
+                backspaceButtonFunction();
+                break;
+            case 'Escape':
+                clearButtonFunction();
+                break;
+            default:
+                if (!isNaN(event.key))
+                    resultTextElement.textContent += event.key;
+                break;
+        }
+    }
+});
+
+function floatPointFunction() {
+    let numberArray = resultTextElement.textContent.split(/[+,-,*,/]/);
+    let lastNumberString = numberArray[numberArray.length - 1];
+    console.log(lastNumberString);
+    if (lastNumberString.includes("."))
+        alert("There is no such number! Please correct it!");
+    else
+        resultTextElement.textContent += ".";
+}
+
+function checkEnding() {
+    if (resultTextElement.textContent.endsWith("/") ||
+        resultTextElement.textContent.endsWith("-") ||
+        resultTextElement.textContent.endsWith("+") ||
+        resultTextElement.textContent.endsWith("*"))
+        return true;
+    return false;
+}
+
+function equalButtonFunction() {
+    if (resultTextElement.textContent.includes("/0") && !resultTextElement.textContent.includes("/0."))
+        alert("You cannot divide by 0. Please retry!");
+    else if (checkEnding())
+        alert("You still have one operation left");
+    else{
+        
+        resultTextElement.textContent = calculate(resultTextElement.textContent);
+
+    }}
+
+function backspaceButtonFunction() {
+    resultTextElement.textContent = resultTextElement.textContent.slice(0, -1);
+}
+
+function clearButtonFunction() {
+    resultTextElement.textContent = '';
+    console.clear();
+}
